c788df07bfc0909bef5e1ea06496ceb72ee0651d
duygu2 <duyguorhan017@gmail.com>
2024-07-01 01:17:07 +0300

    created microservices architecture

---
+++ gatewayserver/src/test/java/com/duygu/gatewayserver/GatewayserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duygu.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
